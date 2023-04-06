9bfd3768483cb8f5c528c5f96e110ea356b7048e
Elis Laxvik <ella0502@storsjogymnasiet.se>
2023-04-06 10:52:56 +0200

    almost done

---
+++ Guess the Number/java.js
@@ -0,0 +1,12 @@
+const slumptal = Math.floor(Math.random() * 9) + 1;
+let antalgissningar = 0;
+
+console.log("slumptal: " + slumptal)
+function gissaTal() {
+    console.log("click")
+    let textfelt_gissa = dokument.getElementById("gissa").value;
+    let gissa_int = Number(textfelt_gissa);
+    let vinn = false;
+    let svar =document.getElementById("svar");
+    console.log("textfelt=" + textfelt_gissa) 
+}
