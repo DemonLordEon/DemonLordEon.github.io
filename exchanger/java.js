041093256b1b68d8004522da6398ea3ebacdec25
Elis Laxvik <ella0502@storsjogymnasiet.se>
2023-06-08 10:16:39 +0200

    fixat sida

--- exchanger/java.js
+++ exchanger/java.js
@@ -24,9 +24,124 @@ function getExchangeArray(pay, price) {
     //500-------------------------------
     money_back = pay - price
     antal_mynt = exChange(money_back, 500)
-    console.log(`antal = ${antal_mynt}` )
+    money_back = money_back % 500
+   
 
+
+let t_money = new money(0, 500)
+
+
+if (antal_mynt > 0) {
+    t_money.antal = antal_mynt
 }
+
+sedlar_mynt_array.push(t_money)
+
+ console.log(`t_money = ${antal_mynt}` )
+
+     //100-------------------------------
+     antal_mynt = exChange(money_back, 100)
+     money_back = money_back % 100
+    
+ 
+ 
+ t_money = new money(0, 100)
+ 
+ 
+ if (antal_mynt > 0) {
+     t_money.antal = antal_mynt
+ }
+ 
+ sedlar_mynt_array.push(t_money)
+ 
+ console.log(`t_money = ${antal_mynt}` )
+
+  //50-------------------------------
+  antal_mynt = exChange(money_back, 50)
+  money_back = money_back % 50
+ 
+
+
+t_money = new money(0, 50)
+
+
+if (antal_mynt > 0) {
+  t_money.antal = antal_mynt
+}
+
+sedlar_mynt_array.push(t_money)
+
+console.log(`t_money = ${antal_mynt}` )
+
+ //20-------------------------------
+ antal_mynt = exChange(money_back, 20)
+ money_back = money_back % 20
+
+
+
+t_money = new money(0, 20)
+
+
+if (antal_mynt > 0) {
+ t_money.antal = antal_mynt
+}
+
+sedlar_mynt_array.push(t_money)
+
+console.log(`t_money = ${antal_mynt}` )
+
+ //10-------------------------------
+ antal_mynt = exChange(money_back, 10)
+ money_back = money_back % 10
+
+
+
+t_money = new money(0, 10)
+
+
+if (antal_mynt > 0) {
+ t_money.antal = antal_mynt
+}
+
+sedlar_mynt_array.push(t_money)
+
+console.log(`t_money = ${antal_mynt}` )
+
+ //5-------------------------------
+ antal_mynt = exChange(money_back, 5)
+ money_back = money_back % 5
+
+
+
+t_money = new money(0, 5)
+
+
+if (antal_mynt > 0) {
+ t_money.antal = antal_mynt
+}
+
+sedlar_mynt_array.push(t_money)
+
+console.log(`t_money = ${antal_mynt}` )
+
+ //1-------------------------------
+ antal_mynt = exChange(money_back, 1)
+ money_back = money_back % 1
+
+
+
+t_money = new money(0, 1)
+
+
+if (antal_mynt > 0) {
+ t_money.antal = antal_mynt
+}
+
+sedlar_mynt_array.push(t_money)
+
+console.log(`t_money = ${antal_mynt}` )
+}
+
 const input_price = document.getElementById("price");
 const input_inbet = document.getElementById("inbet");
 const svardiv = document.getElementById("svar_div");
